5c0156ef90fca0797c7450f33acca9722dc7b624
IdrisseZonard <43299560+IdrisseZonard@users.noreply.github.com>
2018-09-17 22:22:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,14 +8,15 @@ bot.on('ready', function() {
     console.log("Connectedç");
 });
 
-bot.login(process.env.TOKEN);
+bot.login("NDkwMzE1NzU3NzI1NDE3NDcz.DoDLUw.If9Nq8qzMLHDYRXgTmiGL_jlvDY");
+
 
 bot.on('message', message => {
     if (message.content === prefix + "help") {
 
         var help_embed = new Discord.RichEmbed()
-        .setAuthor("Les commandes")
-        .setFooter(message.author.username, message.author.avatarURL)
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter("Billy's BOT V2")
         .setTimestamp()
         .setColor("E26302") //http://www.code-couleur.com
         .addField(":cop: Modération \n \n - Ban | Usage -!ban @user \n - Kick | Usage -!kick @user \n - Mute | ( EN DEV ) \n - Clear | Usage -!clear <nombre> \n", ".")
@@ -29,6 +30,8 @@ bot.on('message', message => {
         console.log("Insulte FDP");
     }
 });
+
+
 bot.on('message', message => {
     let command = message.content.split(" ")[0];
     const args = message.content.slice(prefix.lenght).split(/ +/);
@@ -37,20 +40,25 @@ bot.on('message', message => {
     if (command === prefix + "kick") {
         let modRole = message.guild.roles.find("name", "🎮 Staff 🎮");
         if(!message.member.roles.has(modRole.id)) {
-            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
+    var kickperm_embed = new Discord.RichEmbed()
+    .addField("Tu n'as pas la permission de faire cette commande.", "Tu as besoin de la permission KICK_MEMBERS")
+        return message.channel.send(kickperm_embed);
         }
         if(message.mentions.users.size  === 0) {
-            return message.reply("Merci de mentionner l'utilisateur à expulser.").catch(console.error);
+    var kickuser_embed = new Discord.RichEmbed()
+    .addField("Merci de mentionner l'utiliseur a éjécter", ":hammer_pick:")
+        return message.channel.send(kickuser_embed);
         }
         let kickMember = message.guild.member(message.mentions.users.first());
         if(!kickMember) {
-            return message.reply("Cet utilisateur est introuvable ou impossible à expulser.")
+    var kickintrouvable_embed = new Discord.RichEmbed()
+    .addField("Cet utilisateur est introuvable ou impossible à expulser.")
+        return message.channel.send(kickitrouvable_embed);
         }
         if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
             return message.reply("Je n'ai pas la permission KICK_MEMBERS pour faire ceci.").catch(console.error);
         }
         kickMember.kick().then(member => {
-            message.reply(`${member.user.username} a été expulsé avec succès`).catch(console.error);
             message.guild.channels.find("name", "sanctions").send(`**${member.user.username} a été expulsé du discord par **${message.author.username}`)
         }).catch(console.error)    
     }});
@@ -63,64 +71,53 @@ bot.on('message', message => {
         if (command === prefix + "ban") {
             let modRole = message.guild.roles.find("name", "🎮 Staff 🎮");
             if(!message.member.roles.has(modRole.id)) {
-    var banperm_embed = new Discord.RichEmebd()
+    var banperm_embed = new Discord.RichEmbed()
     .addField("Tu n'as pas la permission de faire cette commande.", "Tu as besoin de la permission BAN_MEMBERS")
         return message.channel.send(banperm_embed);
             }  
             const member = message.mentions.members.first();
-            if (!member) return message.reply("Merci de mentionner l'utilisateur à bannir.");
+            if (!member)
+    var banperm_embed = new Discord.RichEmbed()
+    .setColor("E26302")
+    .addField("Merci de mentionner l'utilisateur à bannir.", "b!ban @user")
+        return message.channel.send(banperm_embed);
             member.ban().then(member => {
-    var ban_embed = new Discord.RichEmbed()
+    var banuser_embed = new Discord.RichEmbed()
+    .setColor("E26302")
     .addField(`**${member.user.username}**`, `A été banni du discord par **${message.author.username}**`)
-                message.reply(`${member.user.usernme} a été banni avec succès`).catch(console.error);
-                message.guild.channels.find("name", "sanctions").send(ban_embed)
+        return message.channel.send(banuser_embed);
+                message.guild.channels.find("name", "sanctions").send(`**${member.user.username} a été banni du discord par **${message.author.username}`)
             }).catch(console.error)
         }});
+
+    bot.on('message', message => {
+
+        if(message.content === prefix + "infodiscord")
+            var embed = new Discord.RichEmbed()
+            .setDescription(":hammer_pick: Information du Discord")
+            .addField("Nom du Discord", message.guild.name)
+            .addField("Crée le", message.guild.createdAt)
+            .addField("Tu as rejoin le", message.member.joinedAt)
+            .addField("Membre Total", message.guild.memberCount)
+            .setColor("00FBDA")
+            message.channel.sendEmbed(embed)
+    });
 
     bot.on("guildMemberAdd", member => {
         member.guild.channels.find("name", "nouveaux").send(`Bienvenue ${member}`);
     });
 
     bot.on('message', message => {
-        
+
+        if(message.content === prefix + "serverlist")
+            message.channel.send(bot.guilds.map(r => r.name + ` | **${r.memberCount}** membres`))
+    });
+
+    bot.on('message', message => {
+
         if(message.content === prefix + "ping")
-        message.delete()
-        message.author.send('Temps de latence avec le serveur: `' + `${message.createdTimestamp - Date.now()}` + ' ms`');
+            message.channel.sendMessage('Temps de latence avec le serveur: `' + `${message.createdTimestamp - Date.now()}` + ' ms`');
     });
-
-    bot.on('guildMemberAdd', member => {
-        
-        var role = member.guild.roles.find('name', '🔊 Membre Communauté 🔊');
-        member.addRole(role)
-    });
-
-    bot.on('message', message => {
-    
-        if(message.content === prefix + "communaute") {
-            var embed = new Discord.RichEmbed()
-            .setAuthor("🛠️ Notre communauté")
-            .setFooter(message.author.username, message.author.avatarURL)
-            .setTimestamp()
-            .addField("Le dicord BillyRP", "https://discord.gg/QuvxPrf")
-            .addField("Le dicord Billy's Pub", "https://discord.gg/78txJyR")
-            .setColor("00FBDA")
-        message.channel.sendEmbed(embed)
-    });
-
-    bot.on('message', message => {
-    
-        if(message.content === prefix + "infodiscord") {
-            var embed = new Discord.RichEmbed()
-            .setAuthor("🛠️ Notre communauté")
-            .setFooter(message.author.username, message.author.avatarURL)
-            .setTimestamp()
-            .addField("Nom du dicord", message.guild.name)
-            .addField("Crée le", message.guild.createdAt)
-            .addField("Tu as rejoin le", message.member.joinedAt)
-            .addField("Utilisateurs sur le dicord", message.guild.memberCount)
-            .setColor("E26302")
-        message.channel.sendEmbed(embed)
-    }});
 
     bot.on('message', message => {
 
@@ -136,14 +133,20 @@ bot.on('message', message => {
       }
     });
 
+    bot.on('guildMemberAdd', member => {
+        var role = member.guild.roles.find('name', '🔊 Membre Communauté 🔊');
+        member.addRole(role)
+    });
+
     bot.on('message', message => {
-    
-        if(message.content === prefix + "partenaire") {
+
+        if(message.content === prefix + "communaute")
             var embed = new Discord.RichEmbed()
-            .setAuthor("🛠️ Les Partenariats")
+            .setAuthor("🛠️ Notre communauté")
             .setFooter(message.author.username, message.author.avatarURL)
             .setTimestamp()
-            .addField(" \n Commande en Développement", " \n Contactez Idrisse")
-            .setColor("E26302")
+            .addField("Le dicord BillyRP", "https://discord.gg/QuvxPrf")
+            .addField("Le dicord Billy's Pub", "https://discord.gg/78txJyR")
+            .setColor("00FBDA")
         message.channel.sendEmbed(embed)
-    }});
+    });
