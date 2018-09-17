d6ed259d1f52a9575ad5729967efa2f2772c4d14
IdrisseZonard <43299560+IdrisseZonard@users.noreply.github.com>
2018-09-17 06:59:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -150,9 +150,3 @@ bot.on('message', message => {
             .setColor("00FBDA")
         message.channel.sendEmbed(embed)
     });
-
-    bot.on('message', message => {
-        if(message.content === prefix + "test") {
-            message.author.createDM().then(channel => {
-                channel.send('Un ptit test');
-    })}});
