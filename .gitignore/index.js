0e9480cbc435c701e03efb8c19b18fd61c879596
IdrisseZonard <43299560+IdrisseZonard@users.noreply.github.com>
2018-09-18 12:42:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -160,7 +160,7 @@ bot.on('message', message => {
     if(message.content[0] === prefix) {
         if(message.content === prefix + 'test') {
 
-            let role = message.guild.roles.find("name", "⚔️Administrateur⚔️")
+            let role = message.guild.roles.find("name", "🗡️Modérateur🗡️")
 
                 message.member.addRole(role)
                 message.reply("Tu as maitenant le role Test")
